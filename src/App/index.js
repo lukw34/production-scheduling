3ce0f5e665ef286eb0db4ed5c663be357abec2f2
lukw34 <lukw34@gmail.com>
2019-01-04 15:18:29 +0100

    Init Project

---
+++ src/App/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">Production Scheduling</header>
+  </div>
+);
+
+export default App;
